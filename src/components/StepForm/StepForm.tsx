e02a8670b628403ea6c4d87a7a1e2ae028b65d5b
IlCarretto <carlorebuffat412@gmail.com>
2023-08-03 15:58:52 +0200

    fixed

--- src/components/StepForm/StepForm.tsx
+++ src/components/StepForm/StepForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef } from "react";
 import { schemaStep1, schemaStep2 } from "./schema/schemaSteps";
 import { ZodType } from "zod";
 import "./index.scss";
